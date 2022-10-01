8ffd226645ff19d4da96f8c8feac187109088be2
PoieoDev <tim@poieo-dev.com>
2022-10-01 14:09:54 -0700

    updated ugettext for django 4

--- pinax/stripe/models.py
+++ pinax/stripe/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.functional import cached_property
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 import stripe
 from jsonfield.fields import JSONField
